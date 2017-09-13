7cf82b839f6661b33a256d4a04dd0eb2bb2b69cb
aa <aa@aa.com>
2017-09-13 17:31:18 +0800

    0913

--- 0912/src/App.vue
+++ 0912/src/App.vue
@@ -1,14 +1,51 @@
 <template>
   <div>
-    <div v-text="helloworld"></div>
+    <mt-header fixed title="固定在顶部"></mt-header>
+    <!--<div v-text="helloworld"></div>-->
+    <!--<ul>-->
+      <!--<li v-for="(item,index) in items" :class="{A:'a',B:'b',C:'c'}[item.score]">{{item.name}}{{index}}</li>-->
+    <!--</ul>-->
+    <!--<header-vue :text="helloworld"></header-vue>-->
+    <!--<footer-vue></footer-vue>-->
+    <!--<button @click="listen">按钮</button>-->
+    <!--<input type="text" v-model="text">-->
+    <!--{{text | reverse}}-->
+
+    <!--路由-->
     <ul>
-      <li v-for="(item,index) in items" :class="{A:'a',B:'b',C:'c'}[item.score]">{{item.name}}{{index}}</li>
+      <li v-for="item in items">
+        <router-link :to="{name:'luyou',query:{id:item.id}}">{{item.name}}</router-link>
+      </li>
     </ul>
-    <header-vue :text="helloworld"></header-vue>
-    <footer-vue></footer-vue>
-    <button @click="listen">按钮</button>
-    <input type="text" v-model="text">
-    {{text | reverse}}
+    <router-view></router-view>
+    <router-view name="headerVue"></router-view>
+    <router-view name="fooderVue"></router-view>
+    <router-view></router-view>
+    <div style="height:200px">
+      <mt-swipe :auto="4000">
+        <mt-swipe-item><div style="height:200px;background-color:blue;"></div></mt-swipe-item>
+        <mt-swipe-item><div style="height:200px;background-color:green;"></div></mt-swipe-item>
+        <mt-swipe-item><div style="height:200px;background-color:yellow;"></div></mt-swipe-item>
+      </mt-swipe>
+    </div>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="外卖">
+        <img slot="icon" src="">
+        外卖
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        <img slot="icon" src="">
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        <img slot="icon" src="">
+        发现
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        <img slot="icon" src="">
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 <script>
@@ -24,7 +61,8 @@
           {id:2,name:"王三",age:16,score:"B"},
           {id:3,name:"李三",age:11,score:"C"}
         ],
-        text:""
+        text:"",
+        selected:""
       }
     },
     methods: { //行为
@@ -55,5 +93,8 @@
 }
 .c{
   background-color: green;
+}
+ul{
+  margin-top: 50px;
 }
 </style>
---
+++ 0912/src/who.vue
@@ -0,0 +1,26 @@
+<template>
+  <input type="text" v-model="id">
+
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data(){
+      return {
+        id:0
+      }
+    },
+    methods: { //行为
+      
+    },
+    components: {
+      //名称 组件对象
+    },
+    created:function(){
+      console.dir(this.$route);
+      this.id=this.$route.query.id;
+    }
+  }
+</script>
+<style>
+
+</style>
